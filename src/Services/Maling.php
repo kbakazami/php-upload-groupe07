7d9dc61f22d87c8c5b54b1a4b6a88eba094a47b0
kbakazami <struys.wyvin@gmail.com>
2021-12-09 09:23:05 +0100

    Todo & Mailing Class

---
+++ src/Services/Maling.php
@@ -0,0 +1,93 @@
+<?php 
+
+namespace App\Services;
+
+class Mailing{
+
+  private $email;
+  private $envoyeur;
+  private $sujet;
+  private $contenu;
+
+
+  /**
+   * Get the value of email
+   */ 
+  public function getEmail()
+  {
+    return $this->email;
+  }
+
+  /**
+   * Set the value of email
+   *
+   * @return  self
+   */ 
+  public function setEmail($email)
+  {
+    $this->email = $email;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of envoyeur
+   */ 
+  public function getEnvoyeur()
+  {
+    return $this->envoyeur;
+  }
+
+  /**
+   * Set the value of envoyeur
+   *
+   * @return  self
+   */ 
+  public function setEnvoyeur($envoyeur)
+  {
+    $this->envoyeur = $envoyeur;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of sujet
+   */ 
+  public function getSujet()
+  {
+    return $this->sujet;
+  }
+
+  /**
+   * Set the value of sujet
+   *
+   * @return  self
+   */ 
+  public function setSujet($sujet)
+  {
+    $this->sujet = $sujet;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of contenu
+   */ 
+  public function getContenu()
+  {
+    return $this->contenu;
+  }
+
+  /**
+   * Set the value of contenu
+   *
+   * @return  self
+   */ 
+  public function setContenu($contenu)
+  {
+    $this->contenu = $contenu;
+
+    return $this;
+  }
+}
+
--- src/Services/UploadFiles.php
+++ src/Services/UploadFiles.php
@@ -2,6 +2,14 @@
 
 namespace App\Services;
 
+/**
+ * TODO: 1. Mettre le chemin de l'image en bdd
+ * TODO: 2. Renommer l'image avc un nom aléatore
+ * TODO: 3. Appeler cette class depuis la class user ---> exmeple changer la photo de profil
+ * TODO: 4. Système de mailing 
+ * 
+ */
+
 class UploadFiles
 {
   public function upload($imageFiles)
