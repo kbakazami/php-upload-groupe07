5a0a01b4fa0e10b6669335f8984408b03ce5e43f
Sofia CHAUDHRY <sofia.chaudhry2606@gmail.com>
2022-05-30 22:05:40 +0200

    Added some links

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,23 +13,7 @@ class IndexController extends AbstractController
   #[Route(path: "/", name: 'index')]
   public function index(EntityManager $em)
   {
-//    var_dump($_FILES['file']);
-    $user = new User();
-
-    $file = ['name' => 'nomImg.png', 'type' => 'image/png', 'tmp_name' => 'C:\Windows\Temp\php9C6D.tmp', 'error' => 'error', 'size' => 1456];
-
-    $user->setName("Bob")
-      ->setFirstName("John")
-      ->setUsername("Bobby")
-      ->setPassword("randompass")
-      ->setEmail("bob@bob.com")
-      ->setBirthDate(new DateTime('1981-02-16'))
-      ->setImgName($_FILES['file']);
-
-    // On demande au gestionnaire d'entités de persister l'objet
-    // Attention, à ce moment-là l'objet n'est pas encore enregistré en BDD
-    $em->persist($user);
-    $em->flush();
+      echo $this->twig->render('base.html.twig');
   }
 
   #[Route(path: "/contact", name: "contact")]
